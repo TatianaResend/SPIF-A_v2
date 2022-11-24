f6d3ff6853086edb3f6b797c0a4fcf5b5e416786
TatianaResend <101273005+TatianaResend@users.noreply.github.com>
2022-11-24 12:41:53 +0000

    Add files via upload

--- Configurations study/MATLAB/Otimizacao_FirstConfiguration.m
+++ Configurations study/MATLAB/Otimizacao_FirstConfiguration.m
@@ -5,7 +5,8 @@ grid on
 hold on
 axis equal
 
-DrawTrapeze (-100,-130,75)
+y_AreaWork = -130;
+DrawTrapeze (-100,y_AreaWork,75)
 
 % Arms configuration:
 x1=-25;     % x initial arm 1
@@ -18,42 +19,42 @@ colors = 'wbg';
 BinM1 = [];
 BinM2 = [];
 
-P_x = 0; P_y = -130;
+P_x = 0; P_y = y_AreaWork;
 
 [H_eix1,h_eix1,P_eix1,...
     H_eix2,h_eix2,P_eix2] = InitRobot(P_x,P_y,x1,x2,L1,L2);
-BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 
 
 pause(1)
-P_x = 100; P_y = -130;
+P_x = 100; P_y = y_AreaWork;
 CurrentRobot(P_x,P_y,x1,x2,L1,L2,H_eix1,h_eix1,P_eix1,H_eix2,h_eix2,P_eix2)
-BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 
 
-P_x = -100; P_y = -130;
+P_x = -100; P_y = y_AreaWork;
 CurrentRobot(P_x,P_y,x1,x2,L1,L2,H_eix1,h_eix1,P_eix1,H_eix2,h_eix2,P_eix2)
-BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 
-P_x = -61.2; P_y = -230;
+P_x = -61.2; P_y = y_AreaWork - 100;
 CurrentRobot(P_x,P_y,x1,x2,L1,L2,H_eix1,h_eix1,P_eix1,H_eix2,h_eix2,P_eix2)
-BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 
-P_x = 61.2; P_y = -230;
+P_x = 61.2; P_y = y_AreaWork - 100;
 CurrentRobot(P_x,P_y,x1,x2,L1,L2,H_eix1,h_eix1,P_eix1,H_eix2,h_eix2,P_eix2)
-BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 
 for i = 1:10
     P_x = randi([-61 61]);
-    P_y = randi([-230 -130]);
+    P_y = randi([(y_AreaWork - 100) y_AreaWork]);
     CurrentRobot(P_x,P_y,x1,x2,L1,L2,H_eix1,h_eix1,P_eix1,H_eix2,h_eix2,P_eix2)
-    BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x+25)];
-    BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-25)];
+    BinM1 = [BinM1 ; abs(P_y) + 3*abs(P_x-x1)];
+    BinM2 = [BinM2 ; abs(P_y) + 3*abs(P_x-x2)];
 end    
 
 BinM12 = [BinM1 BinM2]
